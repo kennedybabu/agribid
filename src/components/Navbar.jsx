aa9c3561a2e2a13dbabd963e0e2e137f6a37fb9d
kennedybabu <kennedynjogu8@gmail.com>
2022-11-22 12:15:43 +0300

    feature: toggle the mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {IoMdClose} from "react-icons/io";
+import {RiMenu3Fill, RiPlantFill} from 'react-icons/ri';
+import { NavLink } from 'react-router-dom';
+
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false)
   return (
-    <div>Navbar</div>
+    <div className='w-full h-[60px] px-4 flex justify-between items-center'>
+      <div className='h-full flex items-center'>
+          <RiPlantFill />
+          <p>agribid</p>
+      </div>
+
+      {/* links */}
+      <div className='hidden md:flex'>
+          <NavLink>Home</NavLink>
+          <NavLink>Products</NavLink>
+          <NavLink>Account</NavLink>
+      </div>
+
+
+      {/* toggle mobile nav */}
+      <div onClick={(e) => setNav(!nav)} className='cursor-pointer'>
+         {nav ? <IoMdClose /> :  <RiMenu3Fill />}
+      </div>
+
+      {nav ? (
+        <div className='hidden  flex-col w-[60%] z-4'>
+            <NavLink>Home</NavLink>
+            <NavLink>Products</NavLink>
+            <NavLink>Account</NavLink>
+        </div>
+      ) : null}
+    </div>
   )
 }
 
