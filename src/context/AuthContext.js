c4ec04b72b857caa88097d31b97d3dad89d29dad
Moses <moses.junior@student.moringaschool.com>
2022-11-23 15:08:20 +0300

    addition of login and sign out functionality

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,16 +1,37 @@
-import { getAuth, signInWithPhoneNumber } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import {set, ref } from "firebase/database";
+import { database } from "../firebaseConfig";
 
-const phoneNumber = getPhoneNumberFromUserInput();
-const appVerifier = window.recaptchaVerifier;
 
 const auth = getAuth();
-signInWithPhoneNumber(auth, phoneNumber, appVerifier)
-    .then((confirmationResult) => {
-        // SMS sent. Prompt user to type the code from the message, then sign the
-        // user in with confirmationResult.confirm(code).
-        window.confirmationResult = confirmationResult;
-        // ...
-    }).catch((error) => {
-        // Error; SMS not sent
-        // ...
+createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        set(ref(database, 'users/' + user.uid), {
+                username: username,
+                email: email,
+            })
+            // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
     });
+
+signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        const user = userCredential.user;
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    })
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        uid = user.uid
+    } else {
+        // User is signed out.
+    }
+})
