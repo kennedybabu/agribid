9af359be1947432295a259f920a3b7bf69b6b175
patrick mugambi <patrick@kali.patrick.com>
2022-11-25 08:32:37 +0300

    add accounts page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>Navbar
+      <Link to="/account">Account</Link>
+    </div>
   )
 }
 
